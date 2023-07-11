d328ee58b0ddb3519cbefb2e3541de97715c4283
Patrick Klein <p.klein@gastromatic.de>
2023-07-11 12:52:42 +0200

    chore: update eslint config

--- src/TransformCustomScalars.ts
+++ src/TransformCustomScalars.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { FieldTypeDefinition, getSchemaDefinition, TypeDefinition } from './getSchemaDefinition';
 
 type StringTransformerFunction = (value: string) => any;
