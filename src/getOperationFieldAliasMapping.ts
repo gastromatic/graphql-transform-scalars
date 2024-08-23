f51c5a9f41516a6d71ebf490afdc7dfb7411fa2d
Nicklas Knell <nicklas.knell@gmail.com>
2024-08-23 16:47:19 +0200

    feat: support fragments in operations

--- src/getOperationFieldAliasMapping.ts
+++ src/getOperationFieldAliasMapping.ts
@@ -1,10 +1,16 @@
 import { parse, SelectionSetNode } from 'graphql/index';
 
-function setSelectionSetAliases(
-  fieldNameByAliasPath: Map<string, string>,
-  selectionSet: SelectionSetNode,
-  currentPath: string,
-): void {
+function setSelectionSetAliases({
+  fieldNameByAliasPath,
+  fragmentFieldNameByAlias,
+  selectionSet,
+  currentPath,
+}: {
+  fieldNameByAliasPath: Map<string, string>;
+  fragmentFieldNameByAlias: Map<string, Map<string, string>>;
+  selectionSet: SelectionSetNode;
+  currentPath: string;
+}): void {
   for (const selection of selectionSet.selections) {
     if (selection.kind === 'Field') {
       const aliasOrName = selection.alias?.value ?? selection.name.value;
@@ -13,11 +19,29 @@ function setSelectionSetAliases(
         fieldNameByAliasPath.set(path, selection.name.value);
       }
       if (selection.selectionSet) {
-        setSelectionSetAliases(fieldNameByAliasPath, selection.selectionSet, path);
+        setSelectionSetAliases({
+          fieldNameByAliasPath,
+          fragmentFieldNameByAlias,
+          selectionSet: selection.selectionSet,
+          currentPath: path,
+        });
       }
     }
     if (selection.kind === 'InlineFragment' && selection.selectionSet) {
-      setSelectionSetAliases(fieldNameByAliasPath, selection.selectionSet, currentPath);
+      setSelectionSetAliases({
+        fieldNameByAliasPath,
+        fragmentFieldNameByAlias,
+        selectionSet: selection.selectionSet,
+        currentPath,
+      });
+    }
+    if (selection.kind === 'FragmentSpread') {
+      const fragmentAliases = fragmentFieldNameByAlias.get(selection.name.value);
+      if (fragmentAliases) {
+        for (const [alias, fieldName] of fragmentAliases.entries()) {
+          fieldNameByAliasPath.set(`${currentPath}.${alias}`, fieldName);
+        }
+      }
     }
   }
 }
@@ -26,14 +50,30 @@ export function getOperationFieldAliasMapping(schema: string): {
   fieldNameByAliasPath: Map<string, string>;
 } {
   const fieldNameByAliasPath = new Map<string, string>();
+  const fragmentFieldNameByAlias = new Map<string, Map<string, string>>();
   const rootNode = parse(schema);
   for (const definition of rootNode.definitions) {
-    if (definition.kind !== 'OperationDefinition') {
+    if (definition.kind === 'FragmentDefinition') {
+      const fragmentAliases = new Map<string, string>();
+      setSelectionSetAliases({
+        fieldNameByAliasPath: fragmentAliases,
+        fragmentFieldNameByAlias,
+        selectionSet: definition.selectionSet,
+        currentPath: '',
+      });
+      fragmentFieldNameByAlias.set(definition.name.value, fragmentAliases);
+    } else if (definition.kind === 'OperationDefinition') {
+      setSelectionSetAliases({
+        fieldNameByAliasPath,
+        fragmentFieldNameByAlias,
+        selectionSet: definition.selectionSet,
+        currentPath: '',
+      });
+    } else {
       throw new Error(
         `operation definitions contain non-operation definition of kind ${definition.kind}`,
       );
     }
-    setSelectionSetAliases(fieldNameByAliasPath, definition.selectionSet, '');
   }
   return { fieldNameByAliasPath };
 }
